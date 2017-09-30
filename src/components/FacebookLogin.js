16b24411500137ae75c1b92ed2b47424487e0796
Rahul Sethuram <rksethuram9@gmail.com>
2017-09-30 15:14:25 -0700

    Create FB Login component

--- src/components/FacebookLogin.js
+++ src/components/FacebookLogin.js
@@ -7,6 +7,10 @@ import Button from 'material-ui/Button'
 import Typography from 'material-ui/Typography'
 import green from 'material-ui/colors/green'
 import Switch from 'material-ui/Switch'
+import Grid from 'material-ui/Grid'
+import TextField from 'material-ui/TextField'
+import SocialIdentityLinker
+  from '../../build/contracts/SocialIdentityLinker.json'
 
 const styles = theme => ({
   card: {
@@ -26,19 +30,26 @@ const styles = theme => ({
     marginBottom: 12,
     color: theme.palette.text.secondary
   },
+  bar: {},
   checked: {
     color: green[500],
     '& + $bar': {
       backgroundColor: green[500]
     }
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  button: {
+    margin: theme.spacing.unit
   }
 })
 
 class FacebookLoginComponent extends Component {
   state = {
-    checkedA: true,
-    checkedB: false,
-    checkedE: true
+    web3detected: false,
+    accountAddress: ''
   }
 
   componentDidMount () {
@@ -63,34 +74,80 @@ class FacebookLoginComponent extends Component {
     })(document, 'script', 'facebook-jssdk')
   }
 
-  responseFacebook (response) {
-    console.log(response)
+  componentWillReceiveProps (nextProps) {
+    if (this.props.web3 !== nextProps.web3 && nextProps.web3) {
+      nextProps.web3.eth.getAccounts((error, accounts) => {
+        this.setState({
+          accountAddress: accounts[0],
+          web3detected: true
+        })
+      })
+    }
   }
+
+  handleClickSetID () {
+    const contract = require('truffle-contract')
+    const socialIdentityLinker = contract(SocialIdentityLinker)
+    socialIdentityLinker.setProvider(this.props.web3.currentProvider)
+    let socialIdentityLinkerInstance
+
+    socialIdentityLinker.deployed().then(instance => {
+      socialIdentityLinkerInstance = instance
+    })
+  }
+
   render () {
     const classes = this.props.classes
-
     return (
       <div>
         <Card className={classes.card}>
           <CardContent>
-            <div
-              className='fb-login-button'
-              data-max-rows='1'
-              data-size='large'
-              data-button-type='continue_with'
-              data-show-faces='false'
-              data-auto-logout-link='false'
-              data-use-continue-as='true'
-            />
-            <Switch
-              classes={{
-                checked: classes.checked,
-                bar: classes.bar
-              }}
-              checked={this.state.checkedE}
-              aria-label='checkedD'
-            />
+            <Grid container direction='column'>
+              <div
+                className='fb-login-button'
+                data-max-rows='1'
+                data-size='large'
+                data-button-type='continue_with'
+                data-show-faces='false'
+                data-auto-logout-link='false'
+                data-use-continue-as='true'
+              />
+              <Grid item xs={12}>
+                <Grid container direction='row'>
+                  <Typography type='body1' className={classes.pos}>
+                    Web3 Status:
+                  </Typography>
+                  <Switch
+                    classes={{
+                      checked: classes.checked,
+                      bar: classes.bar
+                    }}
+                    checked={this.state.web3detected}
+                    aria-label='web3detected'
+                  />
+                </Grid>
+              </Grid>
+              <TextField
+                id='accountAddress'
+                label='Detected Account Address'
+                className={classes.textField}
+                disabled
+                value={this.state.accountAddress}
+                onChange={() => {}}
+                margin='normal'
+              />
+            </Grid>
           </CardContent>
+          <CardActions>
+            <Button
+              raised
+              color='primary'
+              className={classes.button}
+              onClick={this.handleClickSetID}
+            >
+              Set Wallet Address
+            </Button>
+          </CardActions>
         </Card>
       </div>
     )
