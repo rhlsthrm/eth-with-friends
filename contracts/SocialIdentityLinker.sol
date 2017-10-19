9bcd065283cf202be940d28da28925345527a2fc
Rahul Sethuram <rksethuram9@gmail.com>
2017-10-19 14:59:44 -0700

    Fix contract issue, add ethpm

--- contracts/SocialIdentityLinker.sol
+++ contracts/SocialIdentityLinker.sol
@@ -1,40 +1,43 @@
 pragma solidity ^0.4.2;
-import "./oraclizeAPI.sol";
+import "oraclize/usingOraclize.sol";
 import "./strings.sol";
 
 contract SocialIdentityLinker is usingOraclize {
     using strings for *;
     //state variables
     address public owner;
-    uint256 public totalIdentities ;
+    uint256 public totalIdentities;
     mapping (uint256 => address) public facebookIdentity;
+    mapping (bytes32 => address) requestMap;
 
     modifier checkOwner() {require(owner == msg.sender); _ ;}
 
     //events
-    event EventSetIdentity(uint256 facebookId);
+    event EventSetIdentity(uint256 facebookId, address addr);
     event newOraclizeQuery(string description);
     event Hello(string message);
 
     //constructor
-    function SocialIdentityLinker() {
+    function SocialIdentityLinker() public {
         owner = msg.sender;
         totalIdentities = 0;
         Hello("Hello, World!");
     }
 
     //core functions
-    function __callback(bytes32 myid, string result) {
-        if (msg.sender != oraclize_cbAddress()) throw;
+    function __callback(bytes32 myid, string result) public {
+        require(msg.sender == oraclize_cbAddress());
+        require(requestMap[myid] != 0x0);
 
         uint256 facebookId = parseInt(result);
 
-        // set mapping entry at facebook ID to provided data
+        // look up correct address from mapping
         // this operation can create new or overwrite previous data
-        facebookIdentity[facebookId] =  msg.sender;
+        address sender = requestMap[myid];
+        facebookIdentity[facebookId] = sender;
 
         //call event
-        EventSetIdentity(facebookId);
+        EventSetIdentity(facebookId, sender);
 
         totalIdentities++;
     }
@@ -42,8 +45,10 @@ contract SocialIdentityLinker is usingOraclize {
     function setIdentity(
         uint256 facebookId,
         string facebookAccessToken
-    ) payable
+    ) payable public
     {
+        facebookIdentity[facebookId] = msg.sender;
+
         var fbValidationURL = "json(https://graph.facebook.com/me?fields=id&access_token=".toSlice().concat(facebookAccessToken.toSlice());
         fbValidationURL = fbValidationURL.toSlice().concat(").id".toSlice());
         Hello(fbValidationURL);
@@ -52,12 +57,9 @@ contract SocialIdentityLinker is usingOraclize {
             newOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
         } else {
             newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
-            oraclize_query(60, "URL", fbValidationURL);
+            // map sender to query ID so we can look it up when the callback returns
+            bytes32 queryId = oraclize_query(60, "URL", fbValidationURL);
+            requestMap[queryId] = msg.sender;
         }
     }
-
-  //fallout functions
-  function kill() checkOwner() {
-    revert();
-  }
 }
