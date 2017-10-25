c9cc9426d593c6acdce45f04c505116fb43a05b3
Rahul Sethuram <rksethuram9@gmail.com>
2017-10-24 17:00:51 -0700

    Add Github icon to app bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,11 +4,16 @@ import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
+import GithubCircle from 'mdi-material-ui/GithubCircle'
+import IconButton from 'material-ui/IconButton'
 
 const styles = theme => ({
   root: {
     // marginTop: theme.spacing.unit * 3,
     width: '100%'
+  },
+  flex: {
+    flex: 1
   }
 })
 
@@ -16,11 +21,21 @@ function SimpleAppBar (props) {
   const classes = props.classes
   return (
     <div className={classes.root}>
-      <AppBar position='static'>
+      <AppBar position='fixed'>
         <Toolbar>
-          <Typography type='title' color='inherit'>
-            Ethereum Facebook Identity Linker
+          <Typography type='title' color='inherit' className={classes.flex}>
+            ETH With Friends
           </Typography>
+          <IconButton
+            color='contrast'
+            aria-label='GitHub'
+            onClick={() =>
+              window.location.assign(
+                'https://github.com/rhlsthrm/ethereum-social-identity'
+              )}
+          >
+            <GithubCircle />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
